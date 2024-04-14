ce96a1585cb8fbadfcfe343b4d7f5ec599072613
Abhishek Badole <92942952+abhishekbadole8@users.noreply.github.com>
2024-04-14 15:48:49 +0530

    Server hosting platform change

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,8 @@ import Layout from './components/Layout'
 export const UserContext = createContext()
 
 function App() {
-  const API = `https://surl.up.railway.app`
-  // const API = `https://url-shortner-2ruv.onrender.com`
-  // const API = `http://localhost:5001`
+  
+  const API = `https://url-shortner-2ruv.onrender.com` 
 
   const [isLoading, setIsLoading] = useState(false);
   const [token, setToken] = useState(null)
